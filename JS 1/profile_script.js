dc63bb05c068d5cfcad6035fded2625c39489d46
yolladistihany <yollahany@gmail.com>
2025-10-27 09:13:42 +0700

    Add files via upload

---
+++ JS 1/profile_script.js
@@ -0,0 +1,16 @@
+// variabel & tipe data
+let myName = "Yolla Distihany Onerisino";
+const myAge = 16;
+
+// ouput ke console
+console.log("myName");
+console.log("myAge");
+
+// mengubah konten HTML menggunakan innerHTML
+document.getElementById("nama-saya").innerHTML = "Hallo, Saya Yolla Distihany Onerisino"
+document.getElementById("info-diri").innerHTML = "Saya berusia 16 tahun dan saya suka belajar pemrograman"
+
+alert("Anda telah mengklik tombol");
+
+
+
